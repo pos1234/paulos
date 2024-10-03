351e1f260c0b610ce1245e093b47468d8c8c1ea3
Paulos gebeyehu <93011185+pos1234@users.noreply.github.com>
2024-10-03 10:13:17 +0300

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import myImage from '../Images/my-pic.png';
 const Home = ({ scrollToComponent }: any) => {
     const handleDownload = () => {
-        const pdfUrl = 'assets/Paulos_Gebeyehu_Resume.pdf';
+        const pdfUrl = 'assets/Paulos-Gebeyehu-Tefera-Resume.pdf';
         const link = document.createElement('a');
         link.href = pdfUrl;
         link.download = 'Paulos_Gebeyehu_Resume.pdf';
